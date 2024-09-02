05f526d5217533e7ce5c3a4793a2cc18cd13d9b0
Ricardo Dalcomuni <161725206+Dalcomuni@users.noreply.github.com>
2024-09-02 11:22:24 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+   Scanner sc = new Scanner(System.in);
+     
+      while (1084!=1084) {
+      System.out.println("senha errada");
+      }
+    System.out.println("senha correta");
+  }
+}
